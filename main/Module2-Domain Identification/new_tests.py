b0d1ea45293b1c1af915f3ca39ad802a0d47c7af
vikasyadav125 <vikasyad2@gmail.com>
2016-05-15 13:09:58 +0530

    1:09PM 15 May

---
+++ main/Module2-Domain Identification/new_tests.py
@@ -0,0 +1,277 @@
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+print(chr(27) + "[2J")
+print(Fore.YELLOW + 'Loading the scripts ........... ')
+print(Style.RESET_ALL)
+import nltk
+import random
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize,sent_tokenize
+import string
+
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+
+from nltk.classify import ClassifierI
+from statistics import mode
+import codecs
+import os,re,operator
+
+import spacy
+nlp = spacy.en.English()
+
+def checkVector(word):
+	flag = 0
+	w = nlp(word)
+	for token in w:
+		if token.is_stop == True or token.is_space == True or token.is_punct == True or token.like_num == True or len(str(token)) == 1:
+			flag = 1
+		if flag == 0:
+			return True
+		else:
+			return False
+
+
+def removeMultipleNewLines(str):
+	newstr = re.sub('\n+', '\n', str)
+	return newstr
+
+import sys as Sys
+# Print iterations progress
+def printProgress (iteration, total, prefix = '', suffix = '', decimals = 2, barLength = 100):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+    """
+    filledLength    = int(round(barLength * iteration / float(total)))
+    percents        = round(100.00 * (iteration / float(total)), decimals)
+    bar             = '#' * filledLength + '-' * (barLength - filledLength)
+    Sys.stdout.write('%s [%s] %s%s %s\r' % (prefix, bar, percents, '%', suffix)),
+    Sys.stdout.flush()
+    if iteration == total:
+        print("\n")
+
+corpus_folder = [os.path.join(os.getcwd(),'corpus','selected'),os.path.join(os.getcwd(),'corpus','notselected')]
+
+file_dumps = {}
+all_words = []
+all_questions = []
+domain_words = {}
+universe_words = []
+
+#Reading the corpus
+c = 0
+printProgress(c, len(corpus_folder), prefix = 'Loading Corpus into memory :', suffix = 'Complete', barLength = 50)
+for folder in corpus_folder:
+	c = c + 1
+	printProgress(c, len(corpus_folder), prefix = 'Loading Corpus into memory :', suffix = 'Complete', barLength = 50)
+	for i in os.listdir(folder):
+		#For each domain file in the corpus open the file
+		file_absolute_path = os.path.join(folder,i)
+		#If the file is a .store file open and read the file
+		if i.endswith(".store"): 
+			'''
+			file_dumps['Profit and Loss'] = "Question1\nQuestion2" and so on
+			'''
+			file_dumps[i] = removeMultipleNewLines(open(file_absolute_path, "r").read())
+			#For each word in the domain, add it to the all_words list
+			temp = []
+			for word in word_tokenize(file_dumps[i]):
+				all_words.append(word)
+				temp.append(word)
+			'''
+			domain_words will take in all the words used in a domain
+			domain_words['HCF LCM']=['The','and','The','a' .......] 
+			'''	
+			domain_words[i] = temp
+
+#Enhacing the feature vector (Remove punctutations, stop words, like numbers, spaces)
+
+c = 0
+printProgress(c, len(domain_words), prefix = 'Enhancing Machine Vector :', suffix = 'Complete', barLength = 50)
+for domain in domain_words:
+	c = c + 1
+	printProgress(c, len(domain_words), prefix = 'Enhancing Machine Vector :', suffix = 'Complete', barLength = 50)
+	new_word_list = []
+	for word in domain_words[domain]:
+		if checkVector(word) == True:
+			new_word_list.append(word)
+	domain_words[domain] = new_word_list
+
+
+#Cleaning all_word list
+universe_words = all_words
+new_word_list = []
+c = 0
+printProgress(c, len(all_words), prefix = 'Cleaning Nullifiable Vector :', suffix = 'Complete', barLength = 50)
+for word in all_words:
+	c = c + 1
+	printProgress(c, len(all_words), prefix = 'Cleaning Nullifiable Vector :', suffix = 'Complete', barLength = 50)
+	if checkVector(word) == True:
+		new_word_list.append(word)
+all_words = new_word_list
+
+for domain in file_dumps:
+	for question in file_dumps[domain].splitlines():
+		all_questions.append((question.strip(),domain))
+
+print(Fore.YELLOW + 'SUMMARY')
+print(Style.RESET_ALL)
+random.shuffle(all_questions)
+
+print("Length of corpus           : ",len(all_questions))
+print("Total words in the corpus  : ",len(universe_words))
+print("Unique words in the corpus : ",len(set(universe_words)))
+print("Record shuffle status      :  Completed")
+print("Total questions on record  : ",len(all_questions))
+
+#Finalising the feature vector
+FD_domain_words = {}
+for domain in domain_words:
+	FD_domain_words[domain] = nltk.FreqDist(domain_words[domain])
+
+FD_relevant_words = nltk.FreqDist(all_words)
+
+most_commons_ovarall = FD_relevant_words.most_common(500)
+most_common_domainwise = []
+
+for domain,FD in FD_domain_words.items():
+	to_be_appended = FD.most_common(25)
+	for value in to_be_appended:
+		most_common_domainwise.append(value)
+
+'''
+print(most_commons_ovarall)
+print(most_common_domainwise)
+'''
+
+most_common = most_common_domainwise
+
+word_features = []
+for common in most_common:
+	word_features.append(common[0])
+'''
+print("FEATURE VECTOR :: ",word_features)
+'''
+
+def find_features(document):
+	words = set(word_tokenize(document))
+	features = {}
+	for w in word_features:
+		features[w] = (w in words)
+	return features
+
+featuresets = [(find_features(question), category) for (question,category) in all_questions] #(word_tokenized_review,sentiment) for 10000 movie reviews
+
+random.shuffle(featuresets)
+
+training_set = featuresets[50:] #1900 reviews with (review,sentiment)
+testing_set = featuresets[:50] #remaing revies with (review,sentiment)
+
+print("Total size of the data : ",len(featuresets))
+print("Total size of the training data : ",len(training_set))
+print("Total size of the testing  data : ",len(testing_set))
+
+
+
+classifier = nltk.NaiveBayesClassifier.train(training_set) #The classifier is ready with the training set
+
+print("NLTK Original Naive Bayes Algo Accuracy : ",(nltk.classify.accuracy(classifier,testing_set))*100)
+classifier.show_most_informative_features(15)
+
+'''
+
+Training 3 Naive Bayes Classifier and comparing the results
+
+'''
+MNB_classifier = SklearnClassifier(MultinomialNB())
+MNB_classifier.train(training_set)
+print("MNB_classifier Classifier : ",nltk.classify.accuracy(MNB_classifier,testing_set)*100)
+
+#GAU_classifier = SklearnClassifier(GaussianNB())
+#GAU_classifier.train(training_set)
+#print("GAU_classifier Classifier : ",nltk.classify.accuracy(GAU_classifier,testing_set)*100)
+
+BER_classifier = SklearnClassifier(BernoulliNB())
+BER_classifier.train(training_set)
+print("BER_classifier Classifier : ",nltk.classify.accuracy(BER_classifier,testing_set)*100)
+
+'''
+
+Training 2 Regression Based Classifier and comparing the results
+
+'''
+
+LogisticsRegression_classifier = SklearnClassifier(LogisticRegression())
+LogisticsRegression_classifier.train(training_set)
+print("LogisticsRegression_classifier Classifier : ",nltk.classify.accuracy(LogisticsRegression_classifier,testing_set)*100)
+
+SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
+SGDClassifier_classifier.train(training_set)
+print("SGDClassifier_classifier Classifier : ",nltk.classify.accuracy(SGDClassifier_classifier,testing_set)*100)
+
+'''
+
+Training 3 Support Vector Machine Classifier and comparing the results
+
+'''
+
+SVC_classifier = SklearnClassifier(SVC())
+SVC_classifier.train(training_set)
+print("SVC_classifier Classifier : ",nltk.classify.accuracy(SVC_classifier,testing_set)*100)
+
+LinearSVC_classifier = SklearnClassifier(LinearSVC())
+LinearSVC_classifier.train(training_set)
+print("LinearSVC_classifier Classifier : ",nltk.classify.accuracy(LinearSVC_classifier,testing_set)*100)
+
+# NuSVC_classifier = SklearnClassifier(NuSVC())
+# NuSVC_classifier.train(training_set)
+# print("NuSVC_classifier Classifier : ",nltk.classify.accuracy(NuSVC_classifier,testing_set)*100)
+
+
+
+class VoteClassifier(ClassifierI):
+	def __init__(self, *classifiers):
+		self._classifiers = classifiers
+
+	def classify(self, features):
+		votes = []
+		for c in self._classifiers:
+			v = c.classify(features)
+			votes.append(v)
+		return mode(votes)
+
+	def confidence(self, features):
+		votes = []
+		for c in self._classifiers:
+			v = c.classify(features)
+			votes.append(v)
+
+		choice_votes = votes.count(mode(votes))
+		conf = choice_votes / len(votes)
+		return conf
+		
+voted_classifier = VoteClassifier(classifier,
+									MNB_classifier,
+									BER_classifier,
+									LogisticsRegression_classifier,
+									SGDClassifier_classifier,
+									SVC_classifier,
+									LinearSVC_classifier)
+									
+								
+print("Classification:", len(testing_set[0][0])," ----- ",voted_classifier.classify(testing_set[0][0]), "Confidence %:",voted_classifier.confidence(testing_set[0][0])*100)
+print("Classification:", len(testing_set[1][0])," ----- ",voted_classifier.classify(testing_set[1][0]), "Confidence %:",voted_classifier.confidence(testing_set[1][0])*100)
+print("Classification:", len(testing_set[2][0])," ----- ",voted_classifier.classify(testing_set[2][0]), "Confidence %:",voted_classifier.confidence(testing_set[2][0])*100)
+print("Classification:", len(testing_set[3][0])," ----- ",voted_classifier.classify(testing_set[3][0]), "Confidence %:",voted_classifier.confidence(testing_set[3][0])*100)
+print("Classification:", len(testing_set[4][0])," ----- ",voted_classifier.classify(testing_set[4][0]), "Confidence %:",voted_classifier.confidence(testing_set[4][0])*100)
+print("Classification:", len(testing_set[5][0])," ----- ",voted_classifier.classify(testing_set[5][0]), "Confidence %:",voted_classifier.confidence(testing_set[5][0])*100)
+
+		
